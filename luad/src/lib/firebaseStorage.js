aec2914f48823c28506203147e3b687de8de5675
ComsIndeed <enderdragonmaster@gmail.com>
2023-12-22 18:40:33 +0800

    Attempting fixes to build

--- luad/src/lib/firebaseStorage.js
+++ luad/src/lib/firebaseStorage.js
@@ -1,18 +1,9 @@
-import {
-  deleteObject,
-  getDownloadURL,
-  ref,
-  uploadBytes,
-} from "firebase/storage";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { storage } from "../Firebase/firebase";
 import { v4 as UUID } from "uuid";
 
 // Upload thumbnail
-export async function uploadThumbnail(
-  image,
-  name = undefined,
-  isVerbose = false
-) {
+export async function uploadThumbnail(image, name = undefined) {
   const func = async (passedFileName) => {
     const uploadPromise = uploadBytes(
       ref(storage, `thumbnails/${passedFileName}`),
