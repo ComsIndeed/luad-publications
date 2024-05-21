aeeec8be846a8d66e4dff3f45e1a68398e772a0e
ComsIndeed <enderdragonmaster8059@gmail.com>
2024-05-21 18:15:40 +0800

    Added SEO and OpenGraph stuff, untested

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
-
 import icon from "astro-icon";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs(), icon()]
+  site: "https://luad-db.web.app/",
+
+  integrations: [react(), solidJs(), icon(), robotsTxt()],
 });
