0cbeae3f25214fb215f9ecaa435edba1f17643a4
ComsIndeed <enderdragonmaster8059@gmail.com>
2024-05-23 11:37:12 +0800

    Implemented statically generated categories

---
+++ src/lib/config.ts
@@ -0,0 +1,10 @@
+export const paths = {
+  homepage: "/",
+  accountPage: "/account",
+  posts: "/posts/",
+  documentPage: "/posts/:id",
+  adminPage: "/admin",
+  about: "/about",
+};
+
+export const categories = ["featured", "articles", "editorials", "literature"];
